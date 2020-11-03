c65c3a8d784a02c364d43fe28e5eaa923532d332
Michael Lu <lu.michael@outlook.com>
2020-11-03 00:16:48 -0800

    I will reference everthing from here

--- user_definer.py
+++ user_definer.py
@@ -3,10 +3,7 @@ from Grid.GridProcessing import Grid
 from Shapes.ShapesFunctions import *
 
 # Specify the  file that includes dynamic systems
-from dynamics.Humannoid6D_sys1 import *
-from dynamics.DubinsCar4D import *
-import scipy.io as sio
-
+from dynamics.DubinsCar4D2 import *
 import math
 
 """ USER INTERFACES
@@ -19,52 +16,36 @@ import math
 - Run
 """
 
-# Grid field in this order: min_range, max_range, number of dims, grid dimensions, list of periodic dim: starting at 0
-"""g = grid(np.array([-0.5, -1.0, 0.5, -2.0, -math.pi/2, -8.0]), np.array([0.5, 1.0, 1.5, 2.0, math.pi/2, 8.0]), 6, np.array([27, 26, 27, 26, 27, 26]))
+g = Grid(
+    np.array([-5.0, -5.0, -1.0, -math.pi]),
+    np.array([5.0, 5.0, 5.0, math.pi]),
+    4,
+    np.array([60, 60, 50, 50]),
+    [3],
+)
 
 # Define my object
-my_car = Humanoid_6D()
+my_car = DubinsCar4D2()
 
 # Use the grid to initialize initial value function
-Initial_value_f = Rectangle6D(g)
-
-# Look-back length and time step
-lookback_length = 2.0
-t_step = 0.05
-
-tau = np.arange(start = 0, stop = lookback_length + t_step, step = t_step)
-print("Welcome to optimized_dp \n")
-
-# Use the following variable to specify the characteristics of computation
-compMethod = "minVWithVInit"
-my_object  = my_car
-my_shape = Initial_value_f """
-
-g = Grid(np.array([-5.0, -5.0, -1.0, -math.pi]), np.array([5.0, 5.0, 1.0, math.pi]), 4, np.array([40, 40, 50, 50]), [3])
-
-# Define my object
-my_car = DubinsCar4D()
-
-# Use the grid to initialize initial value function
-Initial_value_f = CylinderShape(g, [3,4], np.zeros(4), 1)
+Initial_value_f = CylinderShape(g, [3, 4], np.zeros(4), 1)
 
 # Look-back lenght and time step
 lookback_length = 2.0
 t_step = 0.05
 
 small_number = 1e-5
-tau = np.arange(start = 0, stop = lookback_length + small_number, step = t_step)
+tau = np.arange(start=0, stop=lookback_length + small_number, step=t_step)
 print("Welcome to optimized_dp \n")
+print("")
 
-'''
+"""
 Assign one of the following strings to `compMethod` to specify the characteristics of computation
 "none" -> compute Backward Reachable Set
 "minVwithV0" -> compute Backward Reachable Tube
 "maxVwithVInit" -> compute max V over time
 "minVwithVInit" compute min V over time
-'''
-compMethod = "none"
-my_object  = my_car
+"""
+compMethod = "minVWithV0"
+my_object = my_car
 my_shape = Initial_value_f
-
-
